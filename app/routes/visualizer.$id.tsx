dbd5ca2c0f419cdf866b4c5024da02800d80749e
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2026-02-22 01:33:00 +0200

    implement drag and drop uploader

---
+++ app/routes/visualizer.$id.tsx
@@ -0,0 +1,7 @@
+export default function VisualizerID() {
+  return (
+    <>
+      <div>VisualizerID</div>
+    </>
+  );
+}
---
+++ components/Upload.tsx
@@ -0,0 +1,142 @@
+import { Check, ImageIcon, UploadIcon } from "lucide-react";
+import { useState } from "react";
+import { useOutletContext } from "react-router";
+import {
+  PROGRESS_INCREMENT,
+  PROGRESS_INTERVAL_MS,
+  REDIRECT_DELAY_MS,
+} from "../lib/constanst";
+
+interface UploadProps {
+  onComplete: (base64: string) => void;
+}
+
+export default function Upload({ onComplete }: UploadProps) {
+  const [file, setFile] = useState<File | null>(null);
+  const [progress, setProgress] = useState<number>(0);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const { isSignedIn } = useOutletContext<AuthContext>();
+
+  const processFile = (selectedFile: File) => {
+    if (!isSignedIn) return;
+
+    setFile(selectedFile);
+    setProgress(0);
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const base64 = e.target?.result as string;
+
+      let currentProgress = 0;
+      const interval = setInterval(() => {
+        currentProgress += PROGRESS_INCREMENT;
+        if (currentProgress >= 100) {
+          currentProgress = 100;
+          clearInterval(interval);
+          setTimeout(() => {
+            onComplete(base64);
+          }, REDIRECT_DELAY_MS);
+        }
+        setProgress(currentProgress);
+      }, PROGRESS_INTERVAL_MS);
+    };
+    reader.readAsDataURL(selectedFile);
+  };
+
+  const handleDragEnter = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!isSignedIn) return;
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!isSignedIn) return;
+    setIsDragging(false);
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!isSignedIn) return;
+    setIsDragging(true);
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!isSignedIn) return;
+    setIsDragging(false);
+
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      processFile(e.dataTransfer.files[0]);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isSignedIn) return;
+    if (e.target.files && e.target.files.length > 0) {
+      processFile(e.target.files[0]);
+    }
+  };
+
+  return (
+    <>
+      <div className="upload">
+        {!file ? (
+          <div
+            className={`dropzone ${isDragging ? "is-dragging" : ""}`}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            <input
+              type="file"
+              className="drop-input"
+              accept=".jpg,.jpeg,.png"
+              disabled={!isSignedIn}
+              onChange={handleChange}
+            />
+            <div className="drop-content">
+              <div className="drop-icon">
+                <UploadIcon size={20} />
+              </div>
+              <p>
+                {isSignedIn
+                  ? "Click to upload or just drag and drop"
+                  : "Sign in or sign up with Puter to upload"}
+              </p>
+              <p className="help">Maximum file size 50 MB.</p>
+            </div>
+          </div>
+        ) : (
+          <div className="upload-status">
+            <div className="status-content">
+              <div className="status-icon">
+                {progress >= 100 ? (
+                  <Check className="check" />
+                ) : (
+                  <ImageIcon className="image" />
+                )}
+              </div>
+              <h3>{file?.name}</h3>
+              <div className="progress">
+                <div className="bar" style={{ width: `${progress}%` }} />
+                <p className="status-text">
+                  {" "}
+                  {progress < 100
+                    ? "Analyzing Floor Plan..."
+                    : "Redirecting..."}
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
